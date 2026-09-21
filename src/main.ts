2eea55a61bb076287b570171f644b9088f0e0719
awesomelena <114659215+awesomelena@users.noreply.github.com>
2026-09-21 09:27:50 +0200

    clickable names

--- src/main.ts
+++ src/main.ts
@@ -7,37 +7,42 @@ import { computeLayout, subtreeWidth } from "./layout/positions";
 import { drawUnions, drawParentage } from "./render/lines";
 import { drawPerson } from "./render/person";
 import { showTooltip, moveTooltip, hideTooltip, personLines } from "./render/tooltip";
+import type { PersonId } from "./types";
 
 async function main() {
   const data = await loadData();
 
-  const rootId = "victoria";
-
   const svg = document.createElementNS(SVG_NS, "svg");
-
-  const { childrenOf, spouseOf } = buildFamily(data);
-  const treeWidth = subtreeWidth(rootId, childrenOf, spouseOf);
-  svg.setAttribute("width", String(treeWidth + 40));
   svg.setAttribute("height", "900");
-
   document.querySelector("#app")!.appendChild(svg);
 
-  const positions = computeLayout(data, rootId);
+  function render(rootId: PersonId) {
+    svg.replaceChildren();
+    hideTooltip();
 
-  drawUnions(svg, data, positions);
-  drawParentage(svg, data, positions);
+    const { childrenOf, spouseOf } = buildFamily(data);
+    const treeWidth = subtreeWidth(rootId, childrenOf, spouseOf);
+    svg.setAttribute("width", String(treeWidth + 40));
 
-  for (const person of data.people) {
-    const pos = positions.get(person.id);
-    if (pos === undefined) continue;
+    const positions = computeLayout(data, rootId);
+    drawUnions(svg, data, positions);
+    drawParentage(svg, data, positions);
 
-    const g = drawPerson(svg, data, person, pos);
-    g.addEventListener("mouseenter", (e) =>
-      showTooltip(personLines(data, person), e.clientX, e.clientY)
-    );
-    g.addEventListener("mousemove", (e) => moveTooltip(e.clientX, e.clientY));
-    g.addEventListener("mouseleave", hideTooltip);
+    for (const person of data.people) {
+      const pos = positions.get(person.id);
+      if (pos === undefined) continue;
+
+      const g = drawPerson(svg, data, person, pos);
+      g.addEventListener("mouseenter", (e) =>
+        showTooltip(personLines(data, person), e.clientX, e.clientY)
+      );
+      g.addEventListener("mousemove", (e) => moveTooltip(e.clientX, e.clientY));
+      g.addEventListener("mouseleave", hideTooltip);
+      g.addEventListener("click", () => render(person.id));
+    }
   }
+
+  render("victoria");
 }
 
 main();
