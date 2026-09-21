ad2e70167ae45b99ae8dffb9cd76f8d74b855141
awesomelena <114659215+awesomelena@users.noreply.github.com>
2026-09-21 10:23:26 +0200

    deploy to the github pages

--- src/data/loader.ts
+++ src/data/loader.ts
@@ -1,7 +1,7 @@
 import type { Dataset } from "../types";
 
 export async function loadData(): Promise<Dataset> {
-  const response = await fetch("/data/test.json");
+  const response = await fetch(`${import.meta.env.BASE_URL}data/test.json`);
   const json = await response.json();
   return json as Dataset;
 }
